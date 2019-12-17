033cbad8dfcfa708852abc2228a48a0e0ce2e551
materialofmouse <materialofmouse@gmail.com>
2019-12-17 12:38:23 +0000

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+apt update
+apt upgtade
+apt install -y vim python3 python3-pip
+
 ln -fs $(pwd)/bash/.bashrc ~/.bashrc
 cp -r  $(pwd)/bash/.machine_env ~/
 ln -fs $(pwd)/vim/.vimrc ~/.vimrc
