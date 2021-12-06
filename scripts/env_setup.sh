5d3fc3946e1aed2632a4617f2dd479c1c0aa9578
materialofmouse <materialofmouse@gmail.com>
2021-12-06 15:00:48 +0900

    fix: conflict

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,13 +1,5 @@
-<<<<<<< HEAD:env_setup.sh
 #!/bin/bash
 ln -fs $(pwd)/bash/.bashrc ~/.bashrc
 cp -r  $(pwd)/bash/.machine_env ~/
 ln -fs $(pwd)/vim/.vimrc ~/.vimrc
 ln -fs $(pwd)/vim/.vim ~/.vim
-
-=======
-#!/usr/bin/bash
-sudo apt update
-sudo apt upgtade
-sudo apt install -y vim python3 python3-pip
->>>>>>> a2708408c9b425255aae39fc0fccb80bb29c4a83:install.sh
